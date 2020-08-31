d0b00ab635f0b21dcb49748f40e45d11ef8e16d0
Werry Marwandi <56526533+YgWei@users.noreply.github.com>
2020-08-31 16:45:19 +0800

    Update express.js

--- src/server/express.js
+++ src/server/express.js
@@ -9,9 +9,10 @@ import * as errorHandler from '../middlewares/errorHandler'
 import config from '../config'
 import fs from 'fs'
 import logger from '../logger/system'
+let server
 
 export default {
-  start: async () => {
+  start: () => {
     const app = express()
     app.use(bodyParser.json())
     app.use(errorHandler.bodyParser)
@@ -28,9 +29,9 @@ export default {
       .toString()
       .replace('https://petstore.swagger.io/v2/swagger.json', '/api/v1/swagger.json')
 
-    app.get('/api-docs/index.html', (req, res) => res.send(swaggerIndexContent))
-    app.get('/api-docs', (req, res) => res.redirect('/api-docs/index.html'))
-    app.use('/api-docs', express.static(pathToSwaggerUi))
+    app.get('/swagger-html/index.html', (req, res) => res.send(swaggerIndexContent))
+    app.get('/swagger-html', (req, res) => res.redirect('/swagger-html/index.html'))
+    app.use('/swagger-html', express.static(pathToSwaggerUi))
 
     // API routes
     app.use(routers)
@@ -43,8 +44,14 @@ export default {
       logger.error(`Unhandled error: ${error.message}`)
     })
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Server running on port ${config.port}`)
     })
+  },
+  stop: () => {
+    if (server) {
+      server.close()
+      logger.info('Server stopped.')
+    }
   }
 }
